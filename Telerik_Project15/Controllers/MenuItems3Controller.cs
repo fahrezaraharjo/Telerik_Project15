d2c230c80a14d18fe8d8ca5fe8672e602c9eca73
fahrezaraharjo <fahrezaraharjo92@gmail.com>
2023-09-21 13:20:08 +0700

    Best practice folder structure, create controller MenuItems, create controller Order, create controller OrderItems, layouting

---
+++ Telerik_Project15/Controllers/MenuItems3Controller.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Telerik_Project15.Models;
+
+namespace Telerik_Project15.Controllers
+{
+    public class MenuItems3Controller : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: MenuItems3
+        public async Task<ActionResult> Index()
+        {
+            var menuItems = db.MenuItems.Include(m => m.Restaurant);
+            return View(await menuItems.ToListAsync());
+        }
+
+        // GET: MenuItems3/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = await db.MenuItems.FindAsync(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // GET: MenuItems3/Create
+        public ActionResult Create()
+        {
+            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name");
+            return View();
+        }
+
+        // POST: MenuItems3/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "MenuItemID,RestaurantID,Name,Description,Price")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MenuItems.Add(menuItem);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name", menuItem.RestaurantID);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems3/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = await db.MenuItems.FindAsync(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name", menuItem.RestaurantID);
+            return View(menuItem);
+        }
+
+        // POST: MenuItems3/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "MenuItemID,RestaurantID,Name,Description,Price")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menuItem).State = System.Data.Entity.EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name", menuItem.RestaurantID);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems3/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = await db.MenuItems.FindAsync(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // POST: MenuItems3/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            MenuItem menuItem = await db.MenuItems.FindAsync(id);
+            db.MenuItems.Remove(menuItem);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
